8d6c3482d8e14dc3390ae640ef0b632399561597
Yenifer <guajeyenifer@gmail.com>
2022-01-25 15:45:29 -0500

    Se crea la estructura base del proyecto

---
+++ src/main/java/co/com/sofka/tddavanzado/TddAvanzadoApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.tddavanzado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAvanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddAvanzadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/tddavanzado/TddAvanzadoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.tddavanzado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddAvanzadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
